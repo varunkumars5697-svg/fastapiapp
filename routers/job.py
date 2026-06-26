57f50f7d27701f71d6654c38b206b46757b19ff2
varun kumar <varunkumars5697@gmail.com>
2026-06-26 12:34:45 +0530

    test git

---
+++ routers/job.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router=APIRouter(prefix="/job",tags=["job"])
+
+@router.get("/")
+def read_job():
+    return{"job":"job root"}
+
+@router.get("/{job_id}")
+def read_job(job_id:int):
+    return{"job_id":job_id}
