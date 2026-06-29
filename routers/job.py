a63cafda77de04e394b823d6577efe8a8e1528d3
varun kumar <varunkumars5697@gmail.com>
2026-06-29 10:04:37 +0530

    test git

--- routers/job.py
+++ routers/job.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from schemas.job import JobCreate,JobUpdate
+from models import job
+from sqlalchemy.orm import Session
+from database import get_db,SessionLocal
+from fastapi import HTTPException,status,Depends
+from models import job as job_model
 
 router = APIRouter(prefix="/job",tags=["job"])
 jobs=[]
